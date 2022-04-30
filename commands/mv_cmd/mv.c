983267178942bb4f6e20d1e4e068aa2c0b0dea59
Oier <oieruni22@gmail.com>
2022-04-30 12:22:55 +0200

    mv command working, might need some bug fixing

--- commands/mv_cmd/mv.c
+++ commands/mv_cmd/mv.c
@@ -1,64 +1,37 @@
-#include <dirent.h>
-// Error control.
-#include <errno.h>
-// System flags.
-#include <sys/fcntl.h>
-// Stat!
 #include <sys/stat.h>
-// Stdlib util
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-
-int mv(char *origin_file, char *destination)
-{
-    int i,fd1,fd2;
-    char* buffer[2];
-    //i=link((const char *)origin_file, (const char *)destination);
-    fd1=open(origin_file,O_RDONLY,0777);
-    fd2=creat(destination,0777);
-    //rename((const char *)fd1,(const char *)fd2);
-
-    while(i=read(fd1,buffer,1)>0)
-    {
-        write(fd2,buffer,1);
-
-    }
-    unlink((const char *)origin_file);
-        close(fd1);
-        close(fd2);
-    //if(errno==EEXIST)
-      //  printf("\n ACCESS ERROR \n");
-
-
-    return 1;
-    /*if(i!=0 || j!=0)
-        return -1;
-    else return 1;*/
-}
 
 int main(int argc, char *argv[])
 {
-    //char *err_sys = THE_SYSTEM;
-    //char *glinda = GLINDA;
-
     if (argc != 3)
     {
-        // If the command is used incorrectly, it will teach the user how to use it.
-        //printerr("HAHAHAHAHAHA, useless ape!", err_sys);
-        write(1, "My dear sweet child, do <<mv file.txt new_location>>, please :)\n",strlen("My dear sweet child, do <<mv file.txt new_location>>, please :)\n"));
-
-        return 1;
+        return -1;
     }
-    if (mv(argv[1], argv[2])==-1)
+    else
     {
-        // If there is no directory to reach,
-        //printerr("I cannot reach that place, ape. Move on!", err_sys);
-        write(1,"I cannot move there!\n", strlen("I cannot move there!\n"));
 
-        return 1;
+        char buffer[1024];
+        int fd_origin, fd_dest, chunk;
+        struct stat permissions;
+
+        stat(argv[1], &permissions);
+        fd_origin = open(argv[1], O_RDONLY);
+        fd_dest = open(argv[2], O_RDWR | O_CREAT, permissions.st_mode);
+
+        do
+        {
+            chunk = read(fd_origin, buffer, sizeof(buffer));
+            write(fd_dest, buffer, chunk);
+        } while (chunk != 0);
+
+        close(fd_origin);
+        unlink(argv[1]);
+        close(fd_dest);
+
+        return 0;
     }
-
-    return 0;
 }
