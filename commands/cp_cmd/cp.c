0706ae103bcbc603ff994f67a79051192a10a2e8
Oier <oieruni22@gmail.com>
2022-05-05 16:38:45 +0200

    Changing cp command to print text when moving files to inventory

--- commands/cp_cmd/cp.c
+++ commands/cp_cmd/cp.c
@@ -15,7 +15,7 @@ void cp(char *origin_file, char *dest_file)
     struct stat info;
 
     files[0] = open(origin_file, O_RDONLY);
-    //get rhe permissions of the origin file
+    // get rhe permissions of the origin file
     stat(origin_file, &info);
 
     if (files[0] == -1)
@@ -24,20 +24,20 @@ void cp(char *origin_file, char *dest_file)
         close(files[0]);
         exit(23);
     }
-    //files[1] = open(dest_file, O_RDWR | O_CREAT, 0777);
-    state=link(origin_file, dest_file);
-    //if (files[1] == -1)
-    if(state!=0)
+    // files[1] = open(dest_file, O_RDWR | O_CREAT, 0777);
+    state = link(origin_file, dest_file);
+    // if (files[1] == -1)
+    if (state != 0)
     {
         perror("Error ");
-        write(1,"\n",strlen("\n"));
-        //close(files[1]);
+        write(1, "\n", strlen("\n"));
+        // close(files[1]);
         exit(23);
     }
     else
     {
-        //change the permission of the destination file to be like the origin file
-          chmod(dest_file, info.st_mode);
+        // change the permission of the destination file to be like the origin file
+        chmod(dest_file, info.st_mode);
     }
 
     while ((count = read(files[0], buffer, sizeof(buffer))) != 0)
@@ -47,6 +47,10 @@ int main(int argc, char **argv)
 {
     if (argc == 3)
     {
+        if (!(strcmp(argv[1], "lighter") && strcmp(argv[2], "../.inventory")))
+        {
+            printf("%s", "prueba");
+        }
         cp(argv[1], argv[2]);
         return 1;
     }
