3c523675246781bd7a51657d6ebe014c5455d65e
Oier <oieruni22@gmail.com>
2022-05-13 11:44:56 +0200

    Updating the headers

--- .Global/global.c
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-
-char inv_path[256] = "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/.inventory/";
-char home_path[256] = "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/";
-char log_path[256] = "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/EgyptLog/";
--- .Global/global.h
+++ .Global/global.h
@@ -1,5 +1,4 @@
 
-extern char inv_path[256];
-extern char home_path[256];
-
-
+#define BACKPACK "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/.inventory/"
+#define HOME "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/"
+#define LOG "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/EgyptLog/"
