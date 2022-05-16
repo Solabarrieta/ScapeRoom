d7bf94514552718a014dff6fb12cf990ca0adcaf
Oier <oieruni22@gmail.com>
2022-05-16 17:57:08 +0200

    Changes...

--- functions/history.c
+++ functions/history.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+#include "../.Global/global.h"
 
 int history(int args, char *argv[])
 {
@@ -11,23 +12,23 @@ int history(int args, char *argv[])
     char arg2[20] = "";
     char arg3[20] = "";
 
-    //generate the bin folder path
+    // generate the bin folder path
     char *root_path;
     char home_dir[255];
     getcwd(home_dir, sizeof(home_dir));
-    char *cut=strstr(home_dir,"/ScapeRoom");
-    root_path = (char *)malloc(strlen(home_dir)+strlen("/ScapeRoom/bin/"));
-    strncpy(root_path,home_dir,cut-home_dir);
-    strcat(root_path,"/ScapeRoom/bin/");
+    char *cut = strstr(home_dir, "/ScapeRoom");
+    root_path = (char *)malloc(strlen(home_dir) + strlen("/ScapeRoom/bin/"));
+    strncpy(root_path, home_dir, cut - home_dir);
+    strcat(root_path, "/ScapeRoom/bin/");
     ////////////////////////////////////////////
 
-    int fd = open("/home/k1/github_scaperoom/ScapeRoom/history_log.txt", O_RDWR | O_CREAT, 0777);
+    int fd = open(OIER_HISTORY, O_RDWR | O_CREAT, 0777);
     lseek(fd, 0, SEEK_END);
-    //printf("%s", argv[0]);
+    // printf("%s", argv[0]);
 
     if (args == 3)
     {
-        buffer = (char *)malloc(strlen(argv[0])+strlen(argv[1]) +strlen(argv[2]) +strlen("  \n"));
+        buffer = (char *)malloc(strlen(argv[0]) + strlen(argv[1]) + strlen(argv[2]) + strlen("  \n"));
         strcpy(arg1, argv[0]);
         strcpy(arg2, argv[1]);
         strcpy(arg3, argv[2]);
@@ -37,29 +38,29 @@ int history(int args, char *argv[])
         strcat(buffer, " ");
         strcat(buffer, arg3);
         strcat(buffer, "\n");
-        //printf("%s", buffer);
+        // printf("%s", buffer);
     }
     else if (args == 2)
     {
-        buffer = (char *)malloc(strlen(argv[0])+strlen(argv[1]) +strlen("  \n"));
+        buffer = (char *)malloc(strlen(argv[0]) + strlen(argv[1]) + strlen("  \n"));
         strcpy(arg1, argv[0]);
         strcpy(arg2, argv[1]);
         strcat(buffer, arg1);
         strcat(buffer, " ");
         strcat(buffer, arg2);
         strcat(buffer, "\n");
-        //printf("%s", buffer);
+        // printf("%s", buffer);
     }
     else if (args == 1)
     {
-        buffer = (char *)malloc(strlen(argv[0]) +strlen("  \n"));
+        buffer = (char *)malloc(strlen(argv[0]) + strlen("  \n"));
         strcpy(arg1, argv[0]);
         strcat(buffer, arg1);
         strcat(buffer, "\n");
-        //printf("%s", buffer);
+        // printf("%s", buffer);
     }
 
-    buffer=buffer+strlen(root_path);
+    buffer = buffer + strlen(root_path);
 
     write(fd, buffer, strlen(buffer));
     close(fd);
