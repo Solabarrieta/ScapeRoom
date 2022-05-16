6c47f5e13ce25f69b1a55ef28904fc140e6fe993
Oier <oieruni22@gmail.com>
2022-05-16 17:56:34 +0200

    Adding some paths

--- .Global/global.h
+++ .Global/global.h
@@ -1,8 +1,8 @@
 
 /*#define BACKPACK "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/.inventory/"
-#define HOME "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/"
-#define LOG "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/EgyptLog/"*/
-
+#define HOME "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/"*/
+#define OIER_HISTORY "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/history_log.txt"
+#define OIER_HOME "/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/"
 
 #define BACKPACK "/home/k1/github_scaperoom/ScapeRoom/.inventory/"
 #define HOME "/home/k1/github_scaperoom/ScapeRoom/Egypt/"
