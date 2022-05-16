7b70e99fd50826b9fb1b22c3e105ac5d07abcbef
Oier <oieruni22@gmail.com>
2022-05-16 17:57:36 +0200

    Changing some paths

--- functions/reset.c
+++ functions/reset.c
@@ -9,23 +9,22 @@
 int reset()
 {
     char auxPath[256];
-    char path[256] = HOME;
+    char path[256] = OIER_HOME;
 
     // Creating the main directories
-   /* mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt", S_IRWXU);
+    mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt", S_IRWXU);
     mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/Great_Pyramid", S_IRWXU);
     mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/Great_Pyramid/FirstRoom", S_IRWXU);
     mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom", S_IRWXU);
     mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom/ThirdRoom", S_IRWXU);
-    mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom/ThirdRoom/FinalRoom", S_IRWXU);*/
+    mkdir("/home/oier/Documentos/uni/año2/ios/proyecto/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom/ThirdRoom/FinalRoom", S_IRWXU);
 
-    mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt", S_IRWXU);
+    /*mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt", S_IRWXU);
     mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt/Great_Pyramid", S_IRWXU);
     mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt/Great_Pyramid/FirstRoom", S_IRWXU);
     mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom", S_IRWXU);
     mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom/ThirdRoom", S_IRWXU);
-    mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom/ThirdRoom/FinalRoom", S_IRWXU);
-
+    mkdir("/home/k1/github_scaperoom/ScapeRoom/Egypt/Great_Pyramid/FirstRoom/SecondRoom/ThirdRoom/FinalRoom", S_IRWXU);*/
 
     // Creating the files
     //  First Room
